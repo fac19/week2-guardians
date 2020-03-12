7e9fa6b5f3c353c21b30c025cb351c322dd3dbcd
Alexreid95 <alexreid95@gmail.com>
2020-03-12 13:08:16 +0000

    Fix comment reset Relates #4
    Co-authered-by: Joe <joe.m.jackson@live.co.uk>

--- search.js
+++ search.js
@@ -1,18 +1,16 @@
-let movieForm = document.querySelector("#movie-form");
-let movieInput = document.querySelector("#movie-input");
-let submitBtn = document.querySelector("#movie-submit");
-//submitBtn.preventDefault();
+// let movieForm = document.querySelector("#movie-form");
+// let movieInput = document.querySelector("#movie-input");
+// let submitBtn = document.querySelector("#movie-submit");
 
-submitBtn.addEventListener("click", submitMovie);
+// submitBtn.addEventListener("click", submitMovie);
 
-function submitMovie(event) {  
-    event.preventDefault();
-    // Call fetchReddit function and pass in movieInput as arguement
-    fetchReddit(movieInput.value)
-    // console.log(movieInput.value);
+// function submitMovie(event) {
+//     //Resets comment section, consider changing innerHTML as its not good prac.
+//     redditBox.innerHTML = "";
 
-    fetchRequest(movieInput.value);   
-    
-    // console.log(event.target);
-}
-
+//     event.preventDefault();
+//     // Call fetchReddit function and pass in movieInput as arguement
+//     fetchReddit(movieInput.value)
+//     // Call fetchRequest function and pass in movieInput as arguement
+//     fetchRequest(movieInput.value);
+// }
